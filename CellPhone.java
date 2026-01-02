40b4997256c310ff4543bf499f05933ffccd52dd
Elahe-Vahdati <vahdatielahe@gmail.com>
2026-01-02 01:53:48 -1200

    code cleanup.

--- CellPhone.java
+++ CellPhone.java
@@ -3,6 +3,7 @@ package se.fifth.product;
 import java.math.BigDecimal;
 
 public class CellPhone extends Product {
+
     private int simCount;
 
     public CellPhone(int id, BigDecimal price, String name, int simCount) {
