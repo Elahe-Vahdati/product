8d9613b7b80318e2ed18919bfec2746e10802030
Elahe Vahdati <66535264+Elahe-Vahdati@users.noreply.github.com>
2026-01-04 06:41:29 -1200

    Update CellPhone.java

--- CellPhone.java
+++ CellPhone.java
@@ -6,6 +6,13 @@ public class CellPhone extends Product {
 
     private int simCount;
 
+    public int getSimCount(){
+        return simCount;
+    }
+    public void setSimcount(int simCount){
+        this.simCount = simCount;
+    }
+
     public CellPhone(int id, BigDecimal price, String name, int simCount) {
         super(id, price, name);
         this.simCount = simCount;
