61f723bee1ba3945daec784ad84f414aa00ef6a9
Elahe Vahdati <66535264+Elahe-Vahdati@users.noreply.github.com>
2026-01-04 06:57:43 -1200

    Update Laptop.java

--- Laptop.java
+++ Laptop.java
@@ -5,6 +5,14 @@ import java.math.BigDecimal;
 public class Laptop extends Product {
     private String cpuType;
 
+    public String getCpuType(){
+        return cpuType;
+    }
+
+    public void setCputype(String cpuType){
+        this.cpuType = cputype;
+    }
+
     public Laptop(int id, BigDecimal price, String brand, String cpuType){
         super(id, price, brand);
         this.cpuType = cpuType;
